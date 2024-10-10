b6bebe36820fa048bdac304e0f5193202e953942
Mustapha-Fnach-coder <mustaphafnachjob@gmail.com>
2024-10-10 18:13:27 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import CvGenerator from './CvGenerator';
+
+const App = () => {
+  return (
+    <div>
+      <CvGenerator />
+    </div>
+  );
+};
+
+export default App;
